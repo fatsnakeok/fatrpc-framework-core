9bc34332262e355fd220d316e19acb9af7651521
liuxin <liuxin@izaodao.org>
2022-07-03 15:14:37 +0800

    [fatrpc-framework-interface][feature/02_proxy][M][C:直属领导我][R:二级领导我] 1.创建统一测试类接口

---
+++ fatrpc-framework-interface/src/main/java/org/fatsnake/fatrpc/framework/interfaces/IDataService.java
@@ -0,0 +1,28 @@
+package org.fatsnake.fatrpc.framework.interfaces;
+
+import java.util.List;
+
+/**
+ * @Auther: fatsnake
+ * @Description":  定义测试类接口
+ * @Date:2022/7/3 3:11 下午
+ * Copyright (c) 2022, zaodao All Rights Reserved.
+ */
+public interface IDataService {
+
+    /**
+     * 发送数据
+     * @param body body
+     * @return String
+     */
+    String sendData(String body);
+
+
+    /**
+     * 获取数据
+     *
+     * @return
+     */
+    List<String> getList();
+
+}
