753accb1d189f4b1dcff5350bd24b52f7d41fe45
liuxin <liuxin@izaodao.org>
2022-07-06 17:21:42 +0800

    引入注册中心--基本操作接口定义

--- fatrpc-framework-core/src/main/java/org/fatsnake/fatrpc/framework/core/registy/zookeeper/AbstractRegister.java
+++ fatrpc-framework-core/src/main/java/org/fatsnake/fatrpc/framework/core/registy/zookeeper/AbstractRegister.java
@@ -1,5 +1,10 @@
 package org.fatsnake.fatrpc.framework.core.registy.zookeeper;
 
+import org.fatsnake.fatrpc.framework.core.registy.RegistryService;
+import org.fatsnake.fatrpc.framework.core.registy.URL;
+
+import java.util.List;
+
 /**
  * @Auther: fatsnake
  * @Description": 对一些注册数据做统一的处理
@@ -8,5 +13,55 @@ package org.fatsnake.fatrpc.framework.core.registy.zookeeper;
  * @Date:2022/7/6 13:37
  * Copyright (c) 2022, zaodao All Rights Reserved.
  */
-public class AbstractRegister {
+public abstract class AbstractRegister implements RegistryService {
+
+
+    @Override
+    public void register(URL url) {
+        
+    }
+
+    @Override
+    public void unregister(URL url) {
+
+    }
+
+    @Override
+    public void subscribe(URL url) {
+
+    }
+
+    @Override
+    public void doUnSubscribe(URL url) {
+
+    }
+
+
+    /**
+     * 留给子类扩展
+     * @param url
+     */
+    public abstract void doAfterSubscribe(URL url);
+
+    /**
+     * 留给子类扩展
+     * @param url
+     */
+    public abstract void doBeforeSubscribe(URL url);
+
+
+    /**
+     * 留给子类扩展
+     * @param serviceName
+     * @return
+     */
+    public abstract List<String> getProviderIps(String serviceName);
+
+
+
+
+
+
+
+
 }
