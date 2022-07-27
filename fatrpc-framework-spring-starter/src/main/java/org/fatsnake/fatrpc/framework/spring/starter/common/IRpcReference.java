00802694cb003da5a12aa368a655ccb8aef51211
liuxin <liuxin@izaodao.org>
2022-07-27 17:02:59 +0800

    [fatrpc-framework-core][feature/sringboot_starter][M][C:直属领导我自己][R:二级领导还是我自己] 1.创建注解

---
+++ fatrpc-framework-spring-starter/src/main/java/org/fatsnake/fatrpc/framework/spring/starter/common/IRpcReference.java
@@ -0,0 +1,30 @@
+package org.fatsnake.fatrpc.framework.spring.starter.common;
+
+import jdk.jfr.DataAmount;
+
+
+import java.lang.annotation.*;
+
+
+/**
+ * @Auther: fatsnake
+ * @Description":
+ * @Date:2022/7/27 16:43
+ * Copyright (c) 2022, zaodao All Rights Reserved.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)  // RetentionPolicy.RUNTIME 注解不仅被保存到class文件中，jvm加载class文件之后，仍然存在
+@Documented
+public @interface IRpcReference {
+    String url() default "";
+
+    String group() default "default";
+
+    String serviceToken() default "";
+
+    int timeOut() default 3000;
+
+    int retry() default 1;
+
+    boolean async() default false;
+}
